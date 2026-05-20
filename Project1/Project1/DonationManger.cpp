0589a196bf87320109e3d3cb70077a7d91d86fe7
Adam Do <tomappleseed1@gmail.com>
2026-05-20 13:28:07 -0700

    Update DonationManger.cpp

--- Project1/Project1/DonationManger.cpp
+++ Project1/Project1/DonationManger.cpp
@@ -143,32 +143,32 @@ void DonationManager::printStatistics() const
     	 << "\tNumber of donations: " << donations.getNumOfDonations() << "\n\n";
 }
 
-void DonationManager::printHighestDonor() const 
+void DonationManager::printHighestDonor() const
 {
     double bestAmount = -1.0;
- 	std::vector<std::string> highestDonors;
- 	highestDonors.reverse(donors.size());
- 	for (const auto& donor : donors)
- 	{
-     double total = getDonorTotal(donor.getDonorName());
-     if (total > bestAmount)
-     {
-         bestAmount = total;
-         highestDonors.clear();
-         highestDonors.push_back(donor.getDonorName());
-     }
-     else if (total == bestAmount && bestAmount > 0)
-     {
-         highestDonors.push_back(donor.getDonorName());
-     }
- }
+    std::vector<std::string> highestDonors;
+    highestDonors.reserve(donors.size());
+    for (const auto& donor : donors)
+    {
+        double total = getDonorTotal(donor.getDonorName());
+        if (total > bestAmount)
+        {
+            bestAmount = total;
+            highestDonors.clear();
+            highestDonors.push_back(donor.getDonorName());
+        }
+        else if (total == bestAmount && bestAmount > 0)
+        {
+            highestDonors.push_back(donor.getDonorName());
+        }
+    }
 
- cout << "Highest donor(s):\n";
- for (const auto& donor : donors)
- {
-     cout << "\t" << name << "\n";
- }
- cout << "\tHighest amount donated: $" << bestAmount << endl;
+    cout << "Highest donor(s):\n";
+    for (const auto& name : highestDonors)
+    {
+        cout << "\t" << name << "\n";
+    }
+    cout << "\tHighest amount donated: $" << bestAmount << endl;
 }
 
 void DonationManager::printCollegeWithHighestTotal() const 
